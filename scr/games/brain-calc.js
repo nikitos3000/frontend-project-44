a6e4f4e9c99448136b4ce0d6c4e1d5696fe6f384
nikita <trinevnikita@gmail.com>
2023-05-26 17:32:26 +0300

    bb

--- scr/games/brain-calc.js
+++ scr/games/brain-calc.js
@@ -26,7 +26,7 @@ const game = () => {
   const num2 = getRandomNum(rangemin, rangemax);
   const operator = operators[getRandomOperator(operators)];
   const Quest = `${num1} ${operator} ${num2}.`;
-  const correctanswer = calculate(num1, num2, operator);
+  const correctanswer = calculate(num1, num2, operator).toString();
   console.log(correctanswer);
   return [correctanswer, Quest];
 };
